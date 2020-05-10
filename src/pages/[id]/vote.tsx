8155035b8fb7238378a75ed2944ea74de86fee5c
Dani Sadczenko <dsadczenko@gmail.com>
2020-05-10 13:10:34 -0500

    Removed unnecessary content sections from vote and results

--- src/pages/[id]/vote.tsx
+++ src/pages/[id]/vote.tsx
@@ -100,19 +100,6 @@ export default function Vote() {
                     <div className="poll-container">
                         <PollVote pollId={id?.toString()}/>
                     </div>
-                    <section>
-                        <h2>What is PollCat.io?</h2>
-                        <p>From the folks that brought you straws for polls, PollCat.io is a whole new kind of animal.
-                            We’ve ditched the bloat of servers and relational tables for a sleek, serverless, agile
-                            solution that’s filled with enough buzz words to make any professional happy. </p>
-                        <p>But we’re not here for professionals. No, we’re here for the unbuttoned people of the world
-                            that have super important decisions to make. <a href="#">What should you have for
-                                dinner?</a> <a href="#">What’s the best movie ever made?</a> <a href="#">Which Super
-                                Smash Brothers game reigns supreme?</a> Find out the answers to those questions and more
-                            with PollCat.io.</p>
-                        <p>Like what you see? <a href="#">Buy us a beer</a> or just get back to asking the really
-                            important questions in life.</p>
-                    </section>
                 </ThemeProvider>
             </main>
             <footer>
