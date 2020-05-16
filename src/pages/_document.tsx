d4ce40647cd77704e398ba0b7cf928d12f1cfca7
Dani Sadczenko <dsadczenko@gmail.com>
2020-05-15 22:46:59 -0500

    Removing layout here because it's not doing anything

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 // Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
-import Layout from '../components/Layout';
 
 interface Props {
     styleTags: any
@@ -15,7 +14,6 @@ export default class MyDocument extends Document<Props> {
     // Step 2: Retrieve styles from components in the page
     const page = renderPage((App) => (props) =>
       sheet.collectStyles(<App {...props} />),
-      sheet.collectStyles(<Layout />),
     );
 
     // Step 3: Extract the styles as <style> tags
